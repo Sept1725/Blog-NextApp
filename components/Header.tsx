b911915f5818e3351484e4448795bcc09ebead71
Sept1725 <shun7094vivids@icloud.com>
2025-03-28 14:56:15 +0000

    共通のスタイルでレイアウト調整

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import Logo from "./Logo";
 import Nav from "./Nav";
+import styles from "styles/Header.module.css";
 
 export default function Header() {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ type HeroProps = {
 
 export default function Hero({ title, subtitle, imageOn = false }: HeroProps) {
   return (
-    <div>
+    <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
