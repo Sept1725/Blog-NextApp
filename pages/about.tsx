1fa9e0b84bf6a6d2932a3c5d4da1fdd15bd2427c
Sept1725 <shun7094vivids@icloud.com>
2025-03-29 03:11:24 +0000

    PostBodyコンポーネント作成

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,16 @@
 import Container from "components/Container";
 import Hero from "components/Hero";
+import PostBody from "components/PostBody";
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
-
-      <p>説明1</p>
-      <h2>項目タイトル1</h2>
-      <p>内容1</p>
+      <PostBody>
+        <p>説明1</p>
+        <h2>項目タイトル1</h2>
+        <p>内容1</p>
+      </PostBody>
     </Container>
   );
 }
